091758776329b46a81de810fc497ebd5eac6ea15
Christine Bumpous <bumpouce@gmail.com>
2020-01-17 01:35:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,10 @@ first_number = 1
 second_number = 2
 
 sum = first_number + second_number
+puts sum
 difference = first_number - second_number
+puts difference
 product = first_number * second_number
+puts product
 quotient = first_number / second_number
+puts quotient
